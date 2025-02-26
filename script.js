4ab39fe744b026a7711ff7add7c68c85a0ec5a21
Raju <rajneduri123@gmail.com>
2025-02-26 19:42:27 +0530

    changes commited

--- script.js
+++ script.js
@@ -1,67 +1,107 @@
-console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
-let turn = "X"
-let isgameover = false;
+console.log("Welcome to Tic Tac Toe");
 
-// Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+let turn = "X";
+let isGameOver = false;
+
+const boxes = document.getElementsByClassName("box");
+const boxtexts = document.getElementsByClassName("boxtext");
+const info = document.querySelector(".info");
+const line = document.querySelector(".line");
+const resetBtn = document.getElementById("reset");
+const imgBox = document.querySelector(".imgbox img");
+const container = document.querySelector(".container");
+
+// Turn & Gameover Sounds – ensure these files are correctly located
+let audioTurn = new Audio("ting.mp3");
+let gameoverSound = new Audio("gameover.mp3");
+
+const winCombos = [
+    [0, 1, 2], // Row 1
+    [3, 4, 5], // Row 2
+    [6, 7, 8], // Row 3
+    [0, 3, 6], // Col 1
+    [1, 4, 7], // Col 2
+    [2, 5, 8], // Col 3
+    [0, 4, 8], // Diagonal 1
+    [2, 4, 6], // Diagonal 2
+];
+
+// Switch turn between X and O
+function changeTurn() {
+    return turn === "X" ? "O" : "X";
 }
 
-// Function to check for a win
-const checkWin = ()=>{
-    let boxtext = document.getElementsByClassName('boxtext');
-    let wins = [
-        [0, 1, 2, -13.6, 5, 0],
-        [3, 4, 5, -13.6, 15, 0],
-        [6, 7, 8, -13.6, 25, 0],
-        [0, 3, 6, -23.6, 15, 90],
-        [1, 4, 7, -13.6, 15, 90],
-        [2, 5, 8, -3.6, 15, 90],
-        [0, 4, 8, -13.6, 15, 45],
-        [2, 4, 6, -13.6, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
+/**
+ * Draws a line between the center of two boxes (startBox, endBox)
+ */
+function drawWinLine(startBox, endBox) {
+    const containerRect = container.getBoundingClientRect();
+    const rectStart = boxes[startBox].getBoundingClientRect();
+    const rectEnd = boxes[endBox].getBoundingClientRect();
+
+    const startX = rectStart.left + rectStart.width / 2 - containerRect.left;
+    const startY = rectStart.top + rectStart.height / 2 - containerRect.top;
+    const endX = rectEnd.left + rectEnd.width / 2 - containerRect.left;
+    const endY = rectEnd.top + rectEnd.height / 2 - containerRect.top;
+
+    const diffX = endX - startX;
+    const diffY = endY - startY;
+    const distance = Math.sqrt(diffX * diffX + diffY * diffY);
+    const angle = Math.atan2(diffY, diffX) * (180 / Math.PI);
+
+    line.style.width = distance + "px";
+    line.style.transform = `translate(${startX}px, ${startY}px) rotate(${angle}deg)`;
+}
+
+/**
+ * Checks if there's a winner and updates info text styling accordingly
+ */
+function checkWin() {
+    for (let combo of winCombos) {
+        const [a, b, c] = combo;
+        if (
+            boxtexts[a].innerText !== "" &&
+            boxtexts[a].innerText === boxtexts[b].innerText &&
+            boxtexts[b].innerText === boxtexts[c].innerText
+        ) {
+            info.innerText = boxtexts[a].innerText + " Won!";
+            info.classList.add("winner");
+            isGameOver = true;
+
+            drawWinLine(a, c);
+            imgBox.style.width = "200px"; // Show celebration GIF
+
+            gameoverSound.play().catch(err => console.log("Game over sound error:", err));
+            break;
         }
-    })
+    }
 }
 
-// Game Logic
-// music.play()
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
-    let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            audioTurn.play();
+// Handle clicks on each box
+Array.from(boxes).forEach((box, index) => {
+    box.addEventListener("click", () => {
+        if (!isGameOver && boxtexts[index].innerText === "") {
+            boxtexts[index].innerText = turn;
+            audioTurn.play().catch(err => console.log("Turn sound error:", err));
+
             checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if (!isGameOver) {
+                turn = changeTurn();
+                info.innerText = "Turn for " + turn;
+                info.classList.remove("winner");
+            }
         }
-    })
-})
-
-// Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
     });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+});
 
+// Reset button logic: clear board and reset info styling
+resetBtn.addEventListener("click", () => {
+    Array.from(boxtexts).forEach(btxt => (btxt.innerText = ""));
+    turn = "X";
+    isGameOver = false;
+    info.innerText = "Turn for " + turn;
+    info.classList.remove("winner");
+
+    line.style.width = "0";
+    imgBox.style.width = "0";
+});
